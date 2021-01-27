9719bfedc8a9c8e589df9faa0dd4e694941bc96d
KokosnussDEV <kcvw@icloud.com>
2021-01-27 19:08:31 +0100

    ✨Add understandingsort

---
+++ src/understandingsort/index.js
@@ -0,0 +1,8 @@
+const understandingSort = (A) => {
+    // Just accept what it is (Totally not related to https://www.smbc-comics.com/comics/1611767129-20210127.png)
+    return A;
+};
+
+const unsorted = [94, 72, 28, 27, 38, 83];
+
+console.log(stupidSort(unsorted));
